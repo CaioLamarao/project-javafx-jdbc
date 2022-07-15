19025b33264a434be49fad92f0a0c0a4e773baa4
Caio Lamarão <caiolamarao@outlook.com>
2022-07-15 15:25:49 -0300

    Department ComboBox

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -107,6 +107,7 @@ public class DepartmentListController implements Initializable, DataChangeListen
 			dialogStage.initModality(Modality.WINDOW_MODAL);
 			dialogStage.showAndWait();
 		} catch (IOException e) {
+			e.printStackTrace();
 			Alerts.showAlert("IO Exception", "Error Loadig View", e.getMessage(), AlertType.ERROR);
 		}
 	}
